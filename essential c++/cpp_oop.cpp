0ef90f020c4acb09344125bf9760ea1b3ddd33de
zzobaer09 <zzobaer09@gmail.com>
2026-09-09 22:56:15 +0600

    oop

---
+++ essential c++/cpp_oop.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class Rectangle{
+    private: double length;
+    private: double width;
+    public:
+    Rectangle(double length,double width){
+        this->length = length;
+        this->width = width;
+    }
+    public: double getArea(){
+        return length*width;
+    }
+    public: double getPerimeter(){
+        return 2*(length+width);
+    }
+
+
+};
+
+
+int main(int argc, char const *argv[])
+{
+    Rectangle r(3, 2);
+    cout<<r.getArea()<<endl;
+    cout<<r.getPerimeter()<<endl;
+    
+
+    return 0;
+}
